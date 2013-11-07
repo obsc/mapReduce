9d966e42e53e256b89a64c5ae84cb26acaffb739
Obscuron <Abscuron@gmail.com>
2013-11-07 18:55:32 -0500

    added combine functionality

--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -1,12 +1,19 @@
 open Util
 open Worker_manager
+open Thread_pool
 
 (* TODO implement these *)
 let map kv_pairs map_filename : (string * string) list = 
   failwith "Go back whence you came! Trouble the soul of my Mother no more!"
 
 let combine kv_pairs : (string * string list) list = 
-  failwith "You have been doomed ever since you lost the ability to love."
+  let tbl : (string, string list) Hashtbl.t = Hashtbl.create 10 in
+    let add (k, v : string * string) : unit =
+      if Hashtbl.mem tbl k
+      then Hashtbl.replace tbl k (v::(Hashtbl.find tbl k))
+      else Hashtbl.add tbl k [v] in
+  List.iter add kv_pairs;
+  Hashtbl.fold (fun k v a -> (k, v)::a) tbl []
 
 let reduce kvs_pairs reduce_filename : (string * string list) list =
   failwith "The only thing necessary for evil to triumph is for good men to do nothing"
