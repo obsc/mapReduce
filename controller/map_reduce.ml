f51af29b36c01150f44bf2db3e8839698a04b83a
Obscuron <Abscuron@gmail.com>
2013-11-08 18:19:36 -0500

    added synchronized hashtbl methods and outline of map

--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -2,16 +2,47 @@ open Util
 open Worker_manager
 open Thread_pool
 
-(* TODO implement these *)
-let map kv_pairs map_filename : (string * string) list = 
-  failwith "Go back whence you came! Trouble the soul of my Mother no more!"
+type status =
+  | Unstarted
+  | Attempt of int
+  | Success of (string * string) list
+  | Failure
 
-let combine kv_pairs : (string * string list) list = 
+let set (tbl : ('a, 'b) Hashtbl.t) (m : Mutex.t) (k : 'a) (v : 'b) : unit =
+  Mutex.lock m;
+  Hashtbl.replace tbl k v;
+  Mutex.unlock m
+
+let get (tbl : ('a, 'b) Hashtbl.t) (m : Mutex.t) (k : 'a) : 'b =
+  Mutex.lock m;
+  let v : 'b = Hashtbl.find tbl k in
+  Mutex.unlock m; v
+
+let map kv_pairs map_filename : (string * string) list =
+  let tasks : (int * (string * string)) list =
+    List.mapi (fun id x -> (id, x)) kv_pairs in
+  let tbl : (int, status) Hashtbl.t = Hashtbl.create 10 in
+  let p : pool = create 100 in
+  let m : mapper worker_manager = initialize_mappers map_filename in
+  let rec loop (t : (int * (string * string)) list) : unit =
+    loop t in
+  List.iter (fun (id, kv) -> Hashtbl.add tbl id Unstarted) tasks;
+  loop tasks;
+  clean_up_workers m;
+  destroy p;
+  let append_output (id : int) (s : status) a = match s with
+    | Success l -> l@a
+    | _         -> a in
+  Hashtbl.fold append_output tbl []
+
+(* Combines key value pairs with the same key *)
+let combine kv_pairs : (string * string list) list =
   let tbl : (string, string list) Hashtbl.t = Hashtbl.create 10 in
-    let add (k, v : string * string) : unit =
-      if Hashtbl.mem tbl k
-      then Hashtbl.replace tbl k (v::(Hashtbl.find tbl k))
-      else Hashtbl.add tbl k [v] in
+  (* Appends new value v to the previous list of values at the key k *)
+  let add (k, v : string * string) : unit =
+    if Hashtbl.mem tbl k
+    then Hashtbl.replace tbl k (v::(Hashtbl.find tbl k))
+    else Hashtbl.add tbl k [v] in
   List.iter add kv_pairs;
   Hashtbl.fold (fun k v a -> (k, v)::a) tbl []
 
