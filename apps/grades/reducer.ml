ab5ce5acd7fd291d1aa6f6f58d7ed213840cdc34
bozhou94 <boy.zhou94@gmail.com>
2013-11-09 17:08:46 -0500

    grades implemented

--- apps/grades/reducer.ml
+++ apps/grades/reducer.ml
@@ -1,1 +1,9 @@
-open Util;;
+let (key, values) = Program.get_input() in
+let sorted = List.sort compare (List.map float_of_string values) in
+let med = 
+	let mid = List.length sorted / 2 in
+	(* odd case *)
+	if List.length sorted mod 2 = 1 then List.nth sorted mid
+	(* even case *)
+	else ((List.nth sorted (mid - 1)) +. (List.nth sorted mid)) /. 2 in
+Program.set_output [string_of_float med]
