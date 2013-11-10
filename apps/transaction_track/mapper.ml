f5293ddba8d59c250cf54f856a149ae4a580285a
Obscuron <Abscuron@gmail.com>
2013-11-10 16:47:05 -0500

    fixed transaction track

--- apps/transaction_track/mapper.ml
+++ apps/transaction_track/mapper.ml
@@ -3,37 +3,38 @@ open Int64;;
 let (skey, value) = Program.get_input() in
 let key = int_of_string skey in
 (* Split a single block into many transactions *)
-let parse_block block = 
-	(* Map inputs/outputs to (int * bool * int64) for time, add/wipe, amount *)
-	let rec add_inout trans n_in n_out acc =
-		(* Wipe in coins*)
-		if n_in > 0 then
-			begin
-				match trans with
-				| [] -> acc
-				| id::t -> let nval = Util.marshal (key, false, zero) in
-									 add_inout t (numin - 1) numout ((id,nval)::acc)
-			end
-		(* Add to out coins *)
-		else if n_out > 0 then
-			begin
-				match trans with
-				| id::v::t -> let nval = Util.marshal (key, true, of_string v) in
-											add_inout t 0 (numout - 1) ((id, nval)::acc)
-				| _ -> acc
-			end
-		else (trans, acc) in
-	(* Parse an entire transaction *)
-	let rec parse_trans numtrans trans acc = 
-		if numtrans = 0 then acc
-		else begin
-			match trans with
-			| n_in::n_out::t -> let (rem, vals) = add_inout trans numin numout acc in
-													parse_trans (numtrans - 1) rem vals
-			| _ -> acc
-		end
-	match block with 
-	| [] -> []
-	| numtrans::trans -> parse_trans (int_of_string numtrans) trans [] in 
+let parse_block (block : string list) : (string * string) list =
+  match block with
+  | []              -> []
+  | numtrans::trans -> begin
+    let tot : int = int_of_string numtrans in
+    (* Map in/out to ((int * int) * bool * int64) for time, add/wipe, amount *)
+    let rec add_inout cur trans n_in n_out acc =
+      (* Wipe in coins*)
+      if n_in > 0 then begin
+        match trans with
+        | id::t -> let nval = Util.marshal ((key, cur), false, zero) in
+          add_inout cur t (numin - 1) numout ((id,nval)::acc)
+        | _     -> acc
+      end
+      (* Add to out coins *)
+      else if n_out > 0 then begin
+        match trans with
+        | id::v::t -> let nval = Util.marshal ((key, cur), true, of_string v) in
+          add_inout cur t 0 (numout - 1) ((id, nval)::acc)
+        | _        -> acc
+      end
+      else (trans, acc) in
+    (* Parse an entire transaction *)
+    let rec parse_trans cur trans acc = 
+      if cur >= tot then acc
+      else begin
+        match trans with
+        | n_in::n_out::t -> let (rem, vals) = add_inout cur t n_in n_out acc in
+          parse_trans (cur + 1) rem vals
+        | _              -> acc
+      end in
+    parse_trans 0 trans []
+  end in
 Program.set_output (parse_block (Util.split_spaces value))
 
--- apps/transaction_track/reducer.ml
+++ apps/transaction_track/reducer.ml
@@ -1,10 +1,19 @@
 open Int64;;
 
 let (key, values) = Program.get_input() in
-let trans = List.map (fun d -> Util.unmarshal d) values in
-let helper (reset, acc_val) (time, add, val) = 
-	if time > reset && add then (reset, add acc_val val)
-	else if time > reset then (time, zero)
-	else (reset, acc_val) in
-let (reset, sum) = List.fold_left (helper) (0,zero) trans in
-Program.set_output [to_string sum]
+let get_val (v : string) : (int * int) * bool * int64 = Util.unmarshal v in
+let trans : ((int * int) * bool * int64) list = List.map get_val values in
+let rev_cmp ((t_b1, t_t1), x1, y1) ((t_b2, t_t2), x2, y2) : int =
+  if t_b1 < t_b2 then 1
+  else if t_b1 > t_b2 then -1
+  else if t_t1 < t_t2 then 1
+  else if t_t1 > t_t2 then -1
+  else 0 in
+let rec get_sum trans acc : int64 =
+  match trans with
+  | ((t_b, t_t), a, amount)::t -> begin
+    if a then get_sum t (add acc amount)
+    else acc
+  end
+  | []   -> acc in
+Program.set_output [to_string (get_sum (List.sort rev_cmp trans))]
