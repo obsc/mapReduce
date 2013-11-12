eebf95919aebd7609bf9ada591a362ef4487ed39
bozhou94 <boy.zhou94@gmail.com>
2013-11-12 01:48:19 -0500

    nbody mapper & reducer implemented

--- apps/nbody/mapper.ml
+++ apps/nbody/mapper.ml
@@ -1,6 +1,10 @@
 open Util;;
 open Plane;;
 
+let (key, value) = Program.get_input() in
+
+let (id1, body1, id2, body2) = unmarshal value in
+
 let scale_vector scale (v1, v2) = 
 	(s_times v1 scale, s_times v2 scale) in
 	
@@ -11,9 +15,7 @@ let force (m1, l1, v1) (m2,l2,v2) : vector =
 	let mag = s_divide num denom in
 	scale_vector mag (unit_vector l1 l2) in
 
-let (key, value) = Program.get_input() in
-let helper acc k = 
-  match Util.split_spaces k with
-  | course::grade::etc -> (course, grade)::acc
-  | _ -> acc in
-Program.set_output (List.fold_left helper [] (Util.split_to_class_lst value))
+let accel (m1, l1, v1) body2 = 
+	scale_vector (s_divide 1. m1) (force body1 body2) in
+
+Program.set_output ((id1,accel body1 body2)::[(id2, accel body2 body1)])
