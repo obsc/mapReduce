773197ec375481c426f02c1143d6fa5c3bf0e3ca
bozhou94 <boy.zhou94@gmail.com>
2013-11-14 10:45:11 -0500

    implemented nbody.ml

--- apps/nbody/mapper.ml
+++ apps/nbody/mapper.ml
@@ -3,18 +3,20 @@ open Plane;;
 
 let (key, value) = Program.get_input() in
 
+(* The value read in is a tuple of informations*)
 let (id1, body1, id2, body2) = unmarshal value in
 
 let scale_vector scale (v1, v2) = 
 	(s_times v1 scale, s_times v2 scale) in
 	
-(* Calculates the force on body 1 due to body 2*)
+(* Calculates the gravitational force between two bodies*)
 let force (m1, l1, v1) (m2,l2,v2) : vector =
 	let num = s_times (s_times cBIG_G m2) m1 in
 	let denom = s_times (distance l1 l2) (distance l1 l2) in
 	let mag = s_divide num denom in
 	scale_vector mag (unit_vector l1 l2) in
 
+(* Calculates the acceleration of one body due to the other*)
 let accel (m1, l1, v1) body2 = 
 	scale_vector (s_divide 1. m1) (force body1 body2) in
 
