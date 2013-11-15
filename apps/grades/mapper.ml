ac7c4fb6e4a48213c6cc7605bae7e5f2e02aa74b
Obscuron <Abscuron@gmail.com>
2013-11-14 20:37:13 -0500

    handled non-float inputs

--- apps/grades/mapper.ml
+++ apps/grades/mapper.ml
@@ -1,6 +1,9 @@
 let (key, value) = Program.get_input() in
+let is_float (g : string) : bool =
+    try ignore (float_of_string g); true
+    with e -> false in
 let helper acc k = 
   match Util.split_spaces k with
-  | course::grade::etc -> (course, grade)::acc
+  | course::grade::etc -> if is_float grade then (course, grade)::acc else acc
   | _ -> acc in
 Program.set_output (List.fold_left helper [] (Util.split_to_class_lst value))
